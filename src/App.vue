39ed412100f29632c48656b43e983f513b738739
Braydn Tanner <btanner@Braydns-MacBook-Pro.local>
2023-12-20 13:36:37 -0700

    refactor games, add all games

--- src/App.vue
+++ src/App.vue
@@ -1,101 +1,41 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue';
-import { Flower } from './components/HelloWorld.vue';
-
-const game: Flower[][] = [
-  [
-    { color: 'orange', selected: false },
-    { color: 'orange', selected: false },
-    { color: 'orange', selected: false },
-    { color: 'sky', selected: false },
-    { color: 'sky', selected: false },
-    { color: 'purple', selected: false },
-    { color: 'slate', selected: false },
-    { color: 'yellow', selected: false },
-    { color: 'yellow', selected: false },
-  ],
-  [
-    { color: 'purple', selected: false },
-    { color: 'purple', selected: false },
-    { color: 'pink', selected: false },
-    { color: 'slate', selected: false },
-    { color: 'sky', selected: false },
-    { color: 'purple', selected: false },
-    { color: 'purple', selected: false },
-    { color: 'yellow', selected: false },
-    { color: 'yellow', selected: false },
-  ],
-  [
-    { color: 'purple', selected: false },
-    { color: 'purple', selected: false },
-    { color: 'pink', selected: false },
-    { color: 'orange', selected: false },
-    { color: 'sky', selected: false },
-    { color: 'purple', selected: false },
-    { color: 'orange', selected: false },
-    { color: 'orange', selected: false },
-    { color: 'orange', selected: false },
-  ],
-  [
-    { color: 'slate', selected: false },
-    { color: 'sky', selected: false },
-    { color: 'pink', selected: false },
-    { color: 'orange', selected: false },
-    { color: 'yellow', selected: false },
-    { color: 'yellow', selected: false },
-    { color: 'pink', selected: false },
-    { color: 'pink', selected: false },
-    { color: 'orange', selected: false },
-  ],
-  [
-    { color: 'pink', selected: false },
-    { color: 'sky', selected: false },
-    { color: 'sky', selected: false },
-    { color: 'purple', selected: false },
-    { color: 'purple', selected: false },
-    { color: 'purple', selected: false },
-    { color: 'pink', selected: false },
-    { color: 'pink', selected: false },
-    { color: 'slate', selected: false },
-  ],
-  [
-    { color: 'pink', selected: false },
-    { color: 'sky', selected: false },
-    { color: 'yellow', selected: false },
-    { color: 'pink', selected: false },
-    { color: 'slate', selected: false },
-    { color: 'sky', selected: false },
-    { color: 'yellow', selected: false },
-    { color: 'yellow', selected: false },
-    { color: 'yellow', selected: false },
-  ],
-  [
-    { color: 'purple', selected: false },
-    { color: 'yellow', selected: false },
-    { color: 'yellow', selected: false },
-    { color: 'pink', selected: false },
-    { color: 'sky', selected: false },
-    { color: 'sky', selected: false },
-    { color: 'orange', selected: false },
-    { color: 'slate', selected: false },
-    { color: 'sky', selected: false },
-  ],
-  [
-    { color: 'purple', selected: false },
-    { color: 'slate', selected: false },
-    { color: 'yellow', selected: false },
-    { color: 'pink', selected: false },
-    { color: 'pink', selected: false },
-    { color: 'orange', selected: false },
-    { color: 'orange', selected: false },
-    { color: 'orange', selected: false },
-    { color: 'sky', selected: false },
-  ],
-];
+import { ref } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+import { games, Game } from "./games";
+const game = ref<Game | null>(null);
+function chooseGame(index) {
+  game.value = games[index];
+}
 </script>
 
 <template>
-  <div class="h-full w-full bg-green-100">
-    <HelloWorld :game="game" />
+  <div class="h-screen w-screen bg-green-100">
+    <div v-if="!game">
+      <h1
+        class="text-center text-4xl bold uppercase leading-loose text-green-700"
+      >
+        Bloom
+      </h1>
+      <h2 class="text-center text-2xl bold uppercase text-green-700">Games</h2>
+      <div class="flex gap-1 justify-center mt-4">
+        <div v-for="(game, index) in games" :key="game.Name" class="flex">
+          <button
+            @click="chooseGame(index)"
+            class="bg-white shadow-sm py-2 px-4 text-green-700 rounded border border-green-700 hover:shadow-xl hover:border-green-900 hover:text-green-900 duration-500"
+          >
+            {{ game.Name }}
+          </button>
+        </div>
+      </div>
+    </div>
+    <div v-else>
+      <HelloWorld :game="games[0].Board" />
+      <button
+        @click="game = null"
+        class="bg-white shadow-sm py-2 px-4 text-green-700 rounded border border-green-700 hover:shadow-xl hover:border-green-900 hover:text-green-900 duration-500 absolute right-0 bottom-0 m-1"
+      >
+        Back
+      </button>
+    </div>
   </div>
 </template>
